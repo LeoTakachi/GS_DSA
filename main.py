a72b29dc03889972c4a182b561e849238ef54c14
LeoTakachi <leotakachi27@gmail.com>
2026-05-28 08:00:21 -0300

    Iniciando projeto: Váriaveis e condicionais

---
+++ main.py
@@ -0,0 +1,13 @@
+# Requisitando e guardando dados da missão.
+temperatura = int(input(" - Verificando temperatura da nave -\n"
+                        "       Digite o status atual:"))
+print()
+porcentagem_energia = int(input(" - Verificando porcentagem de energia -\n"
+                        "       Digite o status atual:"))
+print()
+status_comunicação = int(input(" - Verificando status de comunicação -\n"
+                        "       Digite o status atual:"))
+
+
+# Verificação dos dados.
+if temperatura > 80 && porcentagem_energia < 20 && status_comunicação = 0:
